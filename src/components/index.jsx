dba1e471d229100319766c738f191bd2de839e0a
mitsuki <mitsuki3553@gmail.com>
2021-05-17 16:25:51 +0900

    🌱 new component file

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+//自作コンポーネント
+export { HomeLayout } from "src/components/HomeLayout";
+
+//fontAwesome関連
+export { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export { faGithub } from "@fortawesome/free-brands-svg-icons";
+export { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
