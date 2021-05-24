5987cc122de30e8ddd4578db63853575371dea51
mitsuki <mitsuki3553@gmail.com>
2021-05-24 18:58:55 +0900

    🎨 データベーズの名前変更、firestoreからデータ取得のコンポーネント作成

--- src/components/addFirestore.js
+++ src/components/addFirestore.js
@@ -12,11 +12,34 @@ export const addFirestore = (id) => {
     measurementId: "G-7Y322KD43Q",
   };
   if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
-  console.log(firebase.apps);
   const db = firebase.firestore();
-  const collection = db.collection("message");
+  const collection = db.collection("pokemonId");
   collection
-    .add({ message: id })
+    .add({ pokemonId: id })
     .then((doc) => console.log(`${doc.id}success`))
-    .catch((e) => console.log(e));
+    .catch((error) => console.log(error));
+};
+export const getFirestore = async (setPrevPoke) => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDc-CZb-Q4X4dvI3YgykDFsJO6EDvlssxo",
+    authDomain: "try-poke.firebaseapp.com",
+    projectId: "try-poke",
+    storageBucket: "try-poke.appspot.com",
+    messagingSenderId: "441617952571",
+    appId: "1:441617952571:web:b6aca1bb528c7eeba4d11e",
+    measurementId: "G-7Y322KD43Q",
+  };
+  if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+  const collection = db.collection("pokemonId");
+
+  collection.get().then((snapshot) => {
+    let arr = [];
+    snapshot.forEach((doc) => {
+      const a = doc.data().pokemonId;
+      arr = [...arr, a];
+    });
+
+    setPrevPoke(arr);
+  });
 };
