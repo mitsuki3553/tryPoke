d60c10aa46b9f90b53c0c6e2c65896edcbab783c
mitsuki <mitsuki3553@gmail.com>
2021-06-02 16:23:04 +0900

    パス記述変更

--- src/components/addFirestore.js
+++ src/components/addFirestore.js
@@ -3,13 +3,12 @@ import "firebase/firestore";
 
 export const addFirestore = (id) => {
   const firebaseConfig = {
-    apiKey: "AIzaSyDc-CZb-Q4X4dvI3YgykDFsJO6EDvlssxo",
-    authDomain: "try-poke.firebaseapp.com",
-    projectId: "try-poke",
-    storageBucket: "try-poke.appspot.com",
-    messagingSenderId: "441617952571",
-    appId: "1:441617952571:web:b6aca1bb528c7eeba4d11e",
-    measurementId: "G-7Y322KD43Q",
+    apiKey: process.env.NEXT_PUBLIC_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+    appId: process.env.NEXT_PUBLIC_APPID,
   };
   if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
